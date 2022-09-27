bd8fa0d95d2b284f05091663e2cbca7d3bce85c5
Aagam Doshi <aagamdoshi1100@gmail.com>
2022-09-27 19:20:39 +0530

    feat : added validations

--- app.js
+++ app.js
@@ -13,15 +13,21 @@ function clickHandler(){
     var Buying = Buyprice.value;
     var Q = quantity.value;
     var t = Currentprice.value;
-console.log("Buying");
-    Calculator(Buying,Q,t);
-}
+
+        Calculator(Buying,Q,t);
+
+   }
+
 
 
 function Calculator(Buy,Quan,Curr){
 
+    if (Buy != "" && Quan !="" && Curr !=""){
+ 
+
     if (Buy < Curr){
         Profit = (Curr-Buy)*Quan;
+        console.log("ggg");
         ProfitPercentage = (Profit*100/Buy)*Quan;
         var Message = "You profit is "+ Profit+" $ and profit percentage is "+ProfitPercentage;
         var color = "green";
@@ -31,20 +37,24 @@ function Calculator(Buy,Quan,Curr){
         LossPercentage = (Loss/Buy*100)*Quan;
         var Message = "You Loss is "+Loss+" $ and Loss percentage is "+LossPercentage;
         var color = "red";
-        ShowMessage(Message,color2);
+        ShowMessage(Message,color);
     }else{
         var Message = "No Pain no gain";
         var color = "blue";
-        ShowMessage(Message,color1);
+        ShowMessage(Message,color);
     }
-
+}else{
+    var message = "Input fields cannot be null";
+    var color = "#f54211";
+    ShowMessage(message,color);
+}
 }
 
 
 function ShowMessage(message,colorOf){
 
  Output.innerText = message;   
- console.log(colorOf);
+
  Output.style.color = colorOf;
 
 }
